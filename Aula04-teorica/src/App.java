514ba21db8637192b2557ff72c24006c81b88f6f
caperez07 <carolina.perez711@gmail.com>
2022-03-10 11:13:02 -0300

    Aula02-parte2

--- Aula04-teorica/src/App.java
+++ Aula04-teorica/src/App.java
@@ -1,15 +1,15 @@
 public class App {
-    public static void main(String[] args){ 
-        //Declara e instancia um objeto caneta
+    public static void main(String[] args) {
+        // Declara e instancia um objeto caneta
         Caneta c1 = new Caneta();
-        c1.modelo = "BIC";
-        c1.cor = "azul";
-        c1.carga = 100;
-        c1.ponta = 1.0;
-        System.out.println("Minha Caneta: "+c1.modelo);
-        System.out.println("Minha Caneta: "+c1.cor);
-        System.out.println("Minha Caneta: "+c1.carga);
-        System.out.println("Minha Caneta: "+c1.ponta);
+        c1.iniciarCaneta("Azul", "BIC", 1.0);
+
+        Caneta c2 = new Caneta();
+        c2.iniciarCaneta("Vermelho", "Stabillo", 0.4);
+
+
+        c1.escrever("Ola mundo! O batman novo é um bom filme.");
+        c2.escrever("Missão impossível pode mesmo ser o homem de ferro?");
 
 
     }
--- Aula04-teorica/src/Caneta.java
+++ Aula04-teorica/src/Caneta.java
@@ -1,13 +1,40 @@
-public class Caneta{
-    //caracteristicas da caneta - seus atributos
+public class Caneta {
+    // caracteristicas da caneta - seus atributos
     String modelo;
     String cor;
     double ponta;
     int carga;
+    final int CARGA_INICIAL = 100;
 
-    //comportamentos daa canetaa - seus metodos
-    void escrever(String texto){ 
-        System.out.println(texto);
+    // comportamentos da caneta - seus metodos
+    void iniciarCaneta(String cor, String modelo, double ponta) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.carga = CARGA_INICIAL;
     }
+
+    void escrever(String texto) {
+for (int i = 0; i < texto.length(); i++)
+        if(this.carga > 0){
+            System.out.print(texto.charAt(i));
+            this.carga -= 1;
+        } else{
+            System.out.print("\n CANETA SEM CARGA!!");
+            break;
+        
+
+    }
+    System.out.println();
 }
 
+
+
+    String mostrarDados() {
+        return "Modelo:" + this.modelo +
+                " - Cor:" + this.cor +
+                " - Ponta:" + this.ponta +
+                " - Carga:" + this.carga;
+
+    }
+}
