dc5ff5210502347ce8f0510dd36c756994b0ff65
caperez07 <carolina.perez711@gmail.com>
2022-05-12 16:01:05 -0300

    Atividade1

--- Atividade1/src/App.java
+++ Atividade1/src/App.java
@@ -1,10 +1,10 @@
 //Carolina Perez
 //RA: 20.00968-2
 
-package Atividade1.src;
+
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Sistema.executar();
     }
 }
