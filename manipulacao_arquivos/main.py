37c2cfd32df91d3e3ed84142cc499ef0b5dd89ae
caperez07 <carolina.perez711@gmail.com>
2022-08-25 10:57:40 -0300

    Aula18-1

--- manipulacao_arquivos/main.py
+++ manipulacao_arquivos/main.py
@@ -1,11 +1,22 @@
 #Código para escrever dados em um arquivo
 
-arquivo = open("data/sistemas.txt", "a")
-continuar = True
-while continuar:
-    os_name = input("Diga um OS (vazio para sair):")
-    if not os_name:
-        continuar = False
-        continue
-    arquivo.write(os_name+'\n')
-arquivo.close()
+from traceback import print_stack
+
+
+try:
+    arquivo = open("data/sistemas.txt", "a")
+    continuar = True
+    while continuar:
+        os_name = input("Diga um OS (vazio para sair):")
+        if not os_name:
+            continuar = False
+            continue
+        arquivo.write(os_name+'\n')
+    arquivo.close()
+except FileNotFoundError:
+    print("Caminho não existe, favor verificar.")
+except Exception:
+    print("Algo não esperado ocorreu:")
+    print_stack()
+finally:
+    print("Chegamos no final pessoal!")
